636aa88825b1e7777fcad95b49ed0b7bf518e298
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-20 04:33:42 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+  state = { 
+      reviews : '' ,
+      
+  };
+  
+  _fetchDate  = async () => {
+    await fetch(NYT_API_KEY)
+    .then(response => response.json())
+    .then( data => {
+      // this.setState({reviews : data})
+    })
+  }
+  componentDidMount (){
+    _fetchDate();
+            
+    }
+    render() {
+        return (
+            <div className='latest-movie-reviews'>
+                
+            </div>
+        )
+    }
+}
